62bd40b0fc94b1bc11e2d4022a95aca51686f41f
yellow <n1610752@edu.cc.uec.ac.jp>
2020-08-19 18:05:26 +0900

    100件超えてもOKにした

--- youtube.py
+++ youtube.py
@@ -3,33 +3,45 @@ import json
 import settings
 import csv
 
-def print_video_comment(video_id, part='snippet', order='time', text_format='plaintext', max_n=10):
-    params = {
-        'key': API_KEY,
-        'part': part,
-        'videoId': video_id,
-        'order': order,
-        'textFormat': text_format,
-        'maxResults': max_n,
-    }
-    response = requests.get(URL + 'commentThreads', params=params)
-    resource = response.json()
-
-    with open('./test2.json', 'w') as f:
-        json.dump(resource, f, indent=4)
-
+def getVideoComment(video_id, part='snippet', order='time', text_format='plaintext', max_n=100):
+    get_cnt = 0
     comments = [['video_id', 'comment', 'like_cnt', 'reply_cnt']]
-    for comment_info in resource['items']:
-        get_data = []
-        # コメント
-        comment = comment_info['snippet']['topLevelComment']['snippet']['textDisplay']
-        # グッド数
-        like_cnt = comment_info['snippet']['topLevelComment']['snippet']['likeCount']
-        # 返信数
-        reply_cnt = comment_info['snippet']['totalReplyCount']
+    next_page_token = ''
 
-        get_data.extend((video_id, comment, like_cnt, reply_cnt))
-        comments.append(get_data)
+    while (get_cnt < max_n):
+        params = {
+            'key': API_KEY,
+            'part': part,
+            'videoId': video_id,
+            'order': order,
+            'textFormat': text_format,
+            'maxResults': max_n - get_cnt,
+            'pageToken': next_page_token,
+        }
+        response = requests.get(URL + 'commentThreads', params=params)
+        resource = response.json()
+
+        #with open('./test2-2.json', 'w') as f:
+        #    json.dump(resource, f, indent=4)
+
+        for comment_info in resource['items']:
+            get_data = []
+            # コメント
+            comment = comment_info['snippet']['topLevelComment']['snippet']['textDisplay']
+            # グッド数
+            like_cnt = comment_info['snippet']['topLevelComment']['snippet']['likeCount']
+            # 返信数
+            reply_cnt = comment_info['snippet']['totalReplyCount']
+
+            get_data.extend((video_id, comment, like_cnt, reply_cnt))
+            comments.append(get_data)
+
+        # これ以上取得できるコメントがない場合
+        if ('nextPageToken' not in resource):
+            break
+        
+        get_cnt += resource['pageInfo']['totalResults']
+        next_page_token = resource['nextPageToken']
 
     return comments
 
@@ -43,6 +55,5 @@ API_KEY = settings.API_KEY
 video_id = 'QNSR8pl38EM'
 file_path = './comment/' + video_id + '.csv'
 
-comments = print_video_comment(video_id, order='relevance', max_n=10000)
-#print(comments)
+comments = getVideoComment(video_id, order='relevance', max_n=10000)
 store_comment_csv(file_path, comments)
