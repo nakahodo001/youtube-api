5808d22f2a9c0986c6e84f934923857fd40699be
yellow <n1610752@edu.cc.uec.ac.jp>
2021-02-04 21:30:58 +0900

    constをまとめた

---
+++ const.py
@@ -0,0 +1,23 @@
+EMOTION = {
+    'suki': '好',
+    'ikari': '怒',
+    'kowa': '怖',
+    'yasu': '安',
+    'iya': '嫌',
+    'aware': '焦',
+    'takaburi': '昂',
+    'odoroki': '驚',
+    'haji': '恥',
+    'yorokobi': '喜'
+}
+
+EMOTION_ROMA = [
+    'suki', 'ikari', 'kowa', 'yasu', 'iya', 'aware',
+    'takaburi', 'odoroki', 'haji', 'yorokobi'
+]
+
+EMOTION_KANJI = [
+    '好', '怒', '怖', '安', '嫌', '哀', '昂', '驚', '恥', '喜'
+]
+
+EMOTION_CNT = 10
