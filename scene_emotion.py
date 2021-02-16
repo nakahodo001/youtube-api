2192e71309b14f3a9714a5e4fd021fd30d468c96
yellow <n1610752@edu.cc.uec.ac.jp>
2021-02-17 00:13:17 +0900

    snake case

--- scene_emotion.py
+++ scene_emotion.py
@@ -10,14 +10,14 @@ def getCSV(filepath):
 
     return data
 
-def getEmotionPoint(emotions):
+def get_emotion_point(emotions):
     emotion_point = [0] * const.EMOTION_CNT
     for emotion in emotions:
         emotion_point[const.EMOTION_ROMA.index(emotion)] += 1
 
     return emotion_point
 
-def mostSceneEmotions(scene_emotions):
+def most_scene_emotions(scene_emotions):
     scene_most_emotions = {}
     for scene, emotions in scene_emotions.items():
         max_point = max(emotions)
@@ -26,7 +26,7 @@ def mostSceneEmotions(scene_emotions):
 
     return scene_most_emotions
 
-def getSceneEmotions(data):
+def get_scene_emotions(data):
     emotion_analyzer = MLAsk()
     scene_emotions = {}
 
@@ -37,7 +37,7 @@ def getSceneEmotions(data):
         if res['emotion'] is None:
             continue
 
-        emotion_point = getEmotionPoint(res['emotion'])
+        emotion_point = get_emotion_point(res['emotion'])
         if scene[0] in scene_emotions:
             for index, point in enumerate(emotion_point):
                 scene_emotions[scene[0]][index] += point
@@ -45,12 +45,12 @@ def getSceneEmotions(data):
         else :
             scene_emotions[scene[0]] = emotion_point
 
-    return mostSceneEmotions(scene_emotions)
+    return most_scene_emotions(scene_emotions)
 
 
 filename = '4obg8rf3nnQ'
 filepath = './comment/' + filename + '-split.csv'
 
 data = getCSV(filepath)
-scene_emotion = getSceneEmotions(data)
+scene_emotion = get_scene_emotions(data)
 print(scene_emotion)
